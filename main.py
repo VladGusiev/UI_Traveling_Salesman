8568a2d1942e0441d728f93189e4100272bbdf65
Vladyslav Gusiev <vladyslav.gusiev@gmail.com>
2023-10-31 13:10:42 +0100

    addedd roulette parent choice, made generation pass only if new gen is better tan previous. roulette and elitism has even chancew to be chosen

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ import time
 import matplotlib.pyplot as plt
 import random
 
-POPULATION_AMOUNT = 20
+POPULATION_AMOUNT = 500
+CITIES_NUMBER = 0  # will be set in generate_cities()
 # CITIES_ARRAY = []
 
 
 # Initial generation of cities
 def generate_cities():
     cities_array = []
-    # for i in range(random.randrange(20, 40)):
-    for i in range(random.randrange(10, 11)):
+    global CITIES_NUMBER
+    CITIES_NUMBER = random.randrange(20, 40)
+    # CITIES_NUMBER = random.randrange(10, 11)
+    for i in range(CITIES_NUMBER):
         cities_array.append([random.randrange(200), random.randrange(200)])
 
     # print(cities_array)
@@ -26,10 +29,13 @@ def generate_cities():
 # Generation of 1. generation (randomly shuffled list of cities)
 def generate_first_generation(list_of_cities):
     new_generation = []
-    for _ in range(20):
+    for _ in range(POPULATION_AMOUNT):
         random.shuffle(list_of_cities)  # does not return anything
         new_generation.append(Individual(copy.deepcopy(list_of_cities)))
+
     # print(new_generation)
+    # print(len(new_generation))
+
     return new_generation
 
 
@@ -47,12 +53,14 @@ class Individual:
             x2, y2 = self.chromosomes[i]
             distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
             total_length += distance
+        first_and_last_cite_distance = math.sqrt((self.chromosomes[0][0] - self.chromosomes[-1][0]) ** 2 + (self.chromosomes[0][1] - self.chromosomes[-1][1]) ** 2)
+        total_length += first_and_last_cite_distance
 
         return 1/total_length  # closer number to 1 -> shorter road!
 
     # MUTATION -> switching 2 genes
     def mutate(self):
-        mutation_type = random.randrange(1, 3)
+        mutation_type = random.randrange(1, 3, 1)
 
         #  switch genes next to each other
         if mutation_type == 1:
@@ -73,7 +81,6 @@ class Individual:
         random.shuffle(randomised_chromosomes)  # does not return anything
         return randomised_chromosomes
 
-    # TODO REDO HOW OFFSPRING IS CREATED -> duplicated edges are made
     def create_offspring(self, par2):
 
         # chromosome for offspring
@@ -81,13 +88,14 @@ class Individual:
 
         new_blood_chance = random.random()
         # chance to create new blood
-        if new_blood_chance <= 0.2:
+        if new_blood_chance <= 0.1:
             # print(self.get_new_blood())
             child_chromosome = copy.deepcopy(self.get_new_blood())
         else:
             # for gp1, gp2 in zip(self.chromosomes, par2.chromosomes):
-            max_cutout_size = int(POPULATION_AMOUNT / 2)
+            max_cutout_size = int(CITIES_NUMBER / 2)
             cutout_size = random.randrange(1, max_cutout_size)
+            # print(len(self.chromosomes))
 
             cut_out_place = random.randrange(len(self.chromosomes) - cutout_size + 1)
             inherited_part = copy.deepcopy(self.chromosomes[cut_out_place:cut_out_place + cutout_size])
@@ -103,11 +111,24 @@ class Individual:
         return Individual(child_chromosome)
 
 
+def roulette_selection(population):
+    total_fitness = sum(chromosome.fitness for chromosome in population)
+    selected_fitness = random.uniform(0, total_fitness)
+    current_sum_of_fitness = 0
+
+    for i, chromosome in enumerate(population):
+        current_sum_of_fitness += chromosome.fitness
+        if current_sum_of_fitness > selected_fitness:
+            return copy.deepcopy(population[i])
+
+
 def main(points_array):
     population = generate_first_generation(points_array)
     generation = 1
     found = False
-    convergence_counter = 500
+    convergence_counter = 2500
+    all_time_best_fit = 0
+    new_generation = []
     while not found:
 
         # sort the population in decreasing order of fitness score
@@ -119,61 +140,102 @@ def main(points_array):
         # Otherwise generate new offsprings for new generation
         new_generation = []
 
-        # Perform Elitism, that mean 10% of fittest population
-        # goes to the next generation
-        s = int((10 * POPULATION_AMOUNT) / 100)
-        new_generation.extend(population[:s])
+        new_parents_amount = int((10 * POPULATION_AMOUNT) / 100)  # 10% of population
 
-        # From 50% of fittest population, Individuals
-        # will mate to produce offspring
-        s = int((90 * POPULATION_AMOUNT) / 100)
-        for _ in range(s):
-            parent1 = random.choice(population[:10])
-            parent2 = random.choice(population[:10])
-            child = parent1.create_offspring(parent2)
+        choice_of_parent_method = random.randrange(1, 3, 1)
+        if choice_of_parent_method == 1:
+        #  ----------- RULETTE ------------------------
+        # total_fitness = sum(chromosome.fitness for chromosome in population)
+        # for _ in range(new_parents_amount):
+        #     selected_fitness = random.uniform(0, total_fitness)
+        #     current_sum_of_fitness = 0
+        #
+        #     for i, chromosome in enumerate(population):
+        #         current_sum_of_fitness += chromosome.fitness
+        #         if current_sum_of_fitness > selected_fitness:
+        #             new_generation.append(copy.deepcopy(population[i]))
+        #             break
+            parents_for_next_generation = []
+            for _ in range(new_parents_amount):
+                parents_for_next_generation.append(roulette_selection(population))
 
-            # occasional mutation
-            mutation_chance = random.random()
-            if mutation_chance <= 0.4:
-                child.mutate()
+            for _ in range(CITIES_NUMBER):
+                parent1 = random.choice(parents_for_next_generation)
+                parent2 = random.choice(parents_for_next_generation)
+                child = parent1.create_offspring(parent2)
 
-            new_generation.append(copy.deepcopy(child))
+                # occasional mutation
+                mutation_chance = random.random()
+                if mutation_chance <= 0.1:
+                    child.mutate()
+
+                new_generation.append(copy.deepcopy(child))
+        else:
+            #  ----------- ELITISM ------------------------
+            # Perform Elitism, that mean 10% of fittest population
+            # goes to the next generation
+            s = int((10 * POPULATION_AMOUNT) / 100)
+            new_generation.extend(population[:s])
+
+            # From 50% of fittest population, Individuals will mate to produce offspring
+            s = int((90 * POPULATION_AMOUNT) / 100)
+            for _ in range(s):
+                parent1 = random.choice(population[:250])
+                parent2 = random.choice(population[:250])
+                child = parent1.create_offspring(parent2)
+
+                # occasional mutation
+                mutation_chance = random.random()
+                if mutation_chance <= 0.1:
+                    child.mutate()
+
+                new_generation.append(copy.deepcopy(child))
 
         new_generation = sorted(new_generation, key=lambda x: x.fitness, reverse=True)
 
-        if new_generation[0].fitness <= population[0].fitness:
-            convergence_counter -= 1
-        else:
-            convergence_counter = 500
+        if (new_generation[0].fitness > all_time_best_fit) and (new_generation[0].fitness > population[0].fitness):
+            all_time_best_fit = new_generation[0].fitness
+            convergence_counter = 2500
 
             print(f"Generation: {generation}\tString: {population[0].chromosomes}\tFitness: {population[0].fitness}")
-            # for i in range(len(population)):
-            x, y = zip(*population[0].chromosomes)  # Unzip the points into separate x and y arrays
-            plt.plot(x, y, marker='o', linestyle='solid')
-            plt.xlabel('X-Axis')
-            plt.ylabel('Y-Axis')
-            plt.title('Traveling Salesman')
-            plt.grid(True)
-            plt.show()
+            # x, y = zip(*population[0].chromosomes)  # Unzip the points into separate x and y arrays
+            # # connect first and last point of the graph
+            # x = list(x) + [x[0]]
+            # y = list(y) + [y[0]]
+            # plt.plot(x, y, marker='o', linestyle='solid')
+            # plt.xlabel('X')
+            # plt.ylabel('Y')
+            # plt.title('Traveling Salesman Genetic Algorithm')
+            # plt.grid(True)
+            # plt.show()
+            # plt.pause(0.05)
+            population = copy.deepcopy(new_generation)
 
-        population = copy.deepcopy(new_generation)
-
-
-
-
+            # print("new gen is lower")
+            # print("next gen fit: ", new_generation[0].fitness)
+            # print("current gen fit: ", population[0].fitness)
+            # print("all_time BEST: ", all_time_best_fit)
+            # print("------------------")
+        else:
+            convergence_counter -= 1
+            # print("new gen is bigger")
+            # print("next gen fit: ", new_generation[0].fitness)
+            # print("current gen fit: ", population[0].fitness)
+            # print("all_time BEST: ", all_time_best_fit)
+            # print("------------------")
         generation += 1
 
-    print(f"Generation: {generation}\tString: {population[0].chromosomes}\tFitness: {population[0].fitness}")
-
-    # # # for i in range(len(population)):
-    x, y = zip(*population[0].chromosomes)  # Unzip the points into separate x and y arrays
-    # plt.clf()  # Clear the current figure
-    plt.plot(x, y, marker='o', linestyle='solid')
-    plt.xlabel('X-Axis')
-    plt.ylabel('Y-Axis')
-    plt.title('Traveling Salesman')
-    plt.grid(True)
-    plt.show()
+    if (new_generation[0].fitness > all_time_best_fit) and (new_generation[0].fitness > population[0].fitness):
+        print(f"Generation: {generation}\tString: {population[0].chromosomes}\tFitness: {population[0].fitness}")
+        x, y = zip(*population[0].chromosomes)  # Unzip the points into separate x and y arrays
+        x = list(x) + [x[0]]
+        y = list(y) + [y[0]]
+        plt.plot(x, y, marker='o', linestyle='solid')
+        plt.xlabel('X-Axis')
+        plt.ylabel('Y-Axis')
+        plt.title('Traveling Salesman')
+        plt.grid(True)
+        plt.show()
 
     #  ---------------------------------------------
 
